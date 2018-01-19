dcd4f4e82838bdf7b1f088c77e1dd9b608c43afc
Farris <farris.ismati@gmail.com>
2018-01-19 15:27:40 -0500

    Added back css for buttons

--- src/components/topic/topicHeader.js
+++ src/components/topic/topicHeader.js
@@ -63,7 +63,7 @@ class TopicHeader extends Component{
           <h2>{this.props.topic.name}</h2>
           <div className="flex glyphicon-header-holder">
             <Glyphicon onClick={()=>this.toggleState()} glyph="glyphicon glyphicon-plus" />
-            <Glyphicon onClick={()=>this.props.toggleTopic(this.props.topic._id, localStorage.userToken, this.props.state)} glyph="glyphicon glyphicon-remove" />
+            <Glyphicon onClick={()=>this.props.toggleTopic(this.props.topic._id, localStorage.userToken, this.props.state)} glyph="glyphicon glyphicon-minus" />
           </div>
         </div>
       </div>
